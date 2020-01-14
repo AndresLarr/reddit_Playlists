bf0bc899d90e1a891cbde7c19be9c0b81fcecb5e
unknown <alarraza725@gmail.com>
2020-01-14 17:11:24 -0500

    started coding and added pseudocode

--- rpl.py
+++ rpl.py
@@ -7,3 +7,14 @@
 import praw #reddit api wrapper
 import spotipy #spotify wrapper
 import request
+
+
+#Password input to avoid leaking accounts password
+pw = input("Please input password")
+
+
+reddit = praw.Reddit(client_id='snho9d7xZNlVjA',
+                     client_secret='Mv_fK5H2hkAiTNvVAj7YQjr4BM8',
+                     username='PlaylistsBot',
+                     password='nCLXgM2N.RZkKhg',
+                     user_agent='Spotify playlist bot by /u/SamAndre')
