8132f13f6632121db424b64d3f44c60e17705125
unknown <alarraza725@gmail.com>
2020-01-13 22:18:50 -0500

    Added changelog, todo; started pseudocode

--- rpl.py
+++ rpl.py
@@ -1,3 +1,9 @@
-import string 
-import time
-#this is a test
+'''Script to initialize a reddit bot to make spotify playlists from reddit threads
+	based on user suggestions, ie "What are the best songs of the 2010's?", 
+	"What are the essential 90's Pump up songs"
+'''
+
+#Imports
+import praw #reddit api wrapper
+import spotipy #spotify wrapper
+import request
