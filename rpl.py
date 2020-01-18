6086e6f2bde68c00fe9f4ba12cf5c88d266ddb13
Andres Eduardo Larraza <ael86@cornell.edu>
2020-01-17 19:37:59 -0500

    Starting main code, checking to see if E PL

--- rpl.py
+++ rpl.py
@@ -1,6 +1,9 @@
 '''Script to initialize a reddit bot to make spotify playlists from reddit threads
 	based on user suggestions, ie "What are the best songs of the 2010's?", 
 	"What are the essential 90's Pump up songs"
+Abbreviations:
+E := There exists
+PL:= Playlist
 '''
 
 #Imports
@@ -26,7 +29,8 @@ subreddit = reddit.subreddit("AskReddit")
 keyphrase = "!song"
 
 #Initialize Spotipy
-
+spotify = spotipy.Spotify()
+#TODO
 
 #Starting to analyze the comments
 for comment in subreddit.stream.comments():
@@ -36,18 +40,22 @@ for comment in subreddit.stream.comments():
 				adding the songs.
 				Need to see how to differentiate between the parent ID being a 
 				submittion_id or a comment'''
-				try:
-					#Now begin to check parent for song and look it up
-					txt = comment.parent()
-					#from the parent comment check to see if its a song
-					'''
-					process the text to see if it can be a song. 
-					Problems:
-					- If the comment contains more than just the song and artist
-					I will need to try and find the song.
-					-What if the song I add isn't correct
-					(will need to add a second command and edit the playlist)
-					-if there are multiple songs that are similar titles/remakes
-					how can i differentiate'''
-				except:
-					reddit.comment("Sorry, your song was not found on spotify")
+				#check to see if E PL for this thread
+				if comment.parent() is Submission: #Checks to see if we are at a top level comment
+ 					results = spotify.search(q = comment.parent().title, type = "playlist")
+
+					try:
+						#Now begin to check parent for song and look it up
+						txt = comment.parent()
+						#from the parent comment check to see if its a song
+						'''
+						process the text to see if it can be a song. 
+						Problems:
+						- If the comment contains more than just the song and artist
+						I will need to try and find the song.
+						-What if the song I add isn't correct
+						(will need to add a second command and edit the playlist)
+						-if there are multiple songs that are similar titles/remakes
+						how can i differentiate'''
+					except:
+						reddit.comment("Sorry, your song was not found on spotify")
